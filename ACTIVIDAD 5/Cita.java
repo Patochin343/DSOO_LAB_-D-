84a823a804f1998f72f9c43686d0a53a31d00a2c
Manu16-vl <jbravor@unsa.edu.pe>
2025-10-15 23:27:46 -0500

    Update Cita.java
    Se implementa cita

---
+++ ACTIVIDAD 5/Cita.java
@@ -0,0 +1,62 @@
+public class Cita {
+    private int codigoCita;
+    private Paciente paciente;
+    private Doctor doctor;
+    private String fecha;
+    private String estado;
+
+    public Cita(int codigoCita, Paciente paciente, Doctor doctor, String fecha, String estado) {
+        this.codigoCita = codigoCita;
+        this.paciente = paciente;
+        this.doctor = doctor;
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public int getCodigoCita() {
+        return codigoCita;
+    }
+
+    public void setCodigoCita(int codigoCita) {
+        this.codigoCita = codigoCita;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public void mostrarInfo() {
+        System.out.println("Código de cita: " + codigoCita);
+        System.out.println("Paciente: " + paciente.getNombre());
+        System.out.println("Doctor: " + doctor.getNombre());
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Estado: " + estado);
+    }
+}
