83d2361b7135fc5cb0d83099054f25df4ed6de61
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-11-06 00:04:32 -0500

    Se implementa clase Deposito

--- ACTIVIDAD 7/Deposito.java
+++ ACTIVIDAD 7/Deposito.java
@@ -1,3 +1,14 @@
-public class Deposito {
-    
+import java.util.Date;
+public class Deposito extends Transaccion {
+    public Deposito(String idTransaccion, Date fecha, double monto) {
+        super(idTransaccion, fecha, monto)
+    }
+    public void procesar(Cuenta cuenta){
+            cuenta.depositar(getMonto());
+            cuenta.registrarTransaccion(this);
+            System.out.println("Transacción de depósito procesada correctamente");
+    }
+    public String toString(){
+        return "Deposito - Monto: "+getMonto()+" - Fecha: "+getFecha();
+    }
 }
