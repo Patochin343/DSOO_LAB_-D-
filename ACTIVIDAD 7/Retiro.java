c85e3ab7ffe9ed09133ee11cfed7677ca19f2b2b
Manu16-vl <jbravor@unsa.edu.pe>
2025-11-05 23:47:38 -0500

    Subiendo Retiro.java

--- ACTIVIDAD 7/Retiro.java
+++ ACTIVIDAD 7/Retiro.java
@@ -1,3 +1,19 @@
-public class Retiro {
-    
+import java.util.Date;
+public class Retiro extends Transaccion {
+    public Retiro(String idTransaccion, Date fecha, double monto) {
+        super(idTransaccion, fecha, monto);
+    }
+    public void procesar(Cuenta cuenta) {
+        if (cuenta.getSaldo() >= getMonto()) {
+            cuenta.retirar(getMonto());
+            cuenta.registrarTransaccion(this);
+            System.out.println("Transacción de retiro procesada correctamente.");
+        } else {
+            System.out.println("Saldo insuficiente para realizar el retiro.");
+        }
+    }
+    public String toString() {
+        return "Retiro - Monto: " + getMonto() + " - Fecha: " + getFecha();
+    }
 }
+
