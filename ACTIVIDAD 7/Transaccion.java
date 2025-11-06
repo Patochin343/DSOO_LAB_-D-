fb6da0338803f7bf56c3dfc4368daf085972da51
Inyte13 <luisgardaz13@gmail.com>
2025-11-06 01:42:52 -0500

    Enhance Transaccion class with properties and constructor
    Added fields and constructor to the Transaccion class.

--- ACTIVIDAD 7/Transaccion.java
+++ ACTIVIDAD 7/Transaccion.java
@@ -1,3 +1,33 @@
-public class Transaccion {
-    
+import java.util.Date;
+
+public abstract class Transaccion {
+    private String idTransaccion;
+    private Date fecha;
+    private double monto;
+
+    public Transaccion(String idTransaccion, Date fecha, double monto) {
+        this.idTransaccion = idTransaccion;
+        this.fecha = fecha;
+        this.monto = monto;
+    }
+
+    // Método abstracto a implementar por subclases
+    public abstract void procesar(Cuenta cuenta);
+
+    public String getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaccion{idTransaccion='" + idTransaccion + "', fecha=" + fecha + ", monto=" + monto + "}";
+    }
 }
