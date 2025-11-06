fc8a3a7110cc7f97bc6ea4ee34466757e3f85c56
Inyte13 <luisgardaz13@gmail.com>
2025-11-06 01:42:37 -0500

    Extend Empleado class with transaction and client methods

--- ACTIVIDAD 7/Empleado.java
+++ ACTIVIDAD 7/Empleado.java
@@ -1,3 +1,31 @@
-public class Empleado {
-    
+import java.util.Date;
+
+public class Empleado extends Persona {
+    private String idEmpleado;
+    private String cargo;
+
+    public Empleado(String dni, String nombre, String apellido, String direccion, String telefono,
+                    String idEmpleado, String cargo) {
+        super(dni, nombre, apellido, direccion, telefono);
+        this.idEmpleado = idEmpleado;
+        this.cargo = cargo;
+    }
+
+    // Procesar una transacción
+    public void procesarTransaccion(Transaccion t, Cuenta cuenta) {
+        t.procesar(cuenta);
+    }
+
+    // Registrar cliente
+    public void registrarCliente(Cliente c) {
+        System.out.println("Empleado " + getNombre() + " registró al cliente " + c.getNombre());
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
 }
