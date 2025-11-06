1936d7745d911965b13f162b2dd2dbf8222ce5d8
Jesus Huertas <jhuertasi@unsa.edu.pe>
2025-11-05 23:42:38 -0500

    Add attributes and methods to Persona class

--- ACTIVIDAD 7/Persona.java
+++ ACTIVIDAD 7/Persona.java
@@ -1,3 +1,35 @@
 public class Persona {
-    
+
+    // Atributos 
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+
+    // Constructor para inicializar los datos
+    public Persona(String dni, String nombre, String apellido, String direccion, String telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("--- Datos Personales ---");
+        System.out.println("DNI: " + this.dni);
+        System.out.println("Nombre: " + this.nombre + " " + this.apellido);
+        System.out.println("Dirección: " + this.direccion);
+        System.out.println("Teléfono: " + this.telefono);
+    }
+
+    // Getters y Setters 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
 }
